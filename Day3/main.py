c8ae53f566d6ac56324f4f591de5f42ba706deaa
Alessio Rosiello <alex.robassomero@gmail.com>
2022-12-03 12:00:57 +0100

    Add Day3 python

---
+++ Day3/main.py
@@ -0,0 +1,10 @@
+SAMPLE_DATA: bool = False
+if SAMPLE_DATA:
+    filename = "sample_data.txt"
+else:
+    filename = "data.txt"
+with open(filename) as f:
+    data = f.read().splitlines()
+part1 = sum(['abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.index(list(set(a).intersection(b))[0]) + 1 for a, b in [(line[:len(line) // 2], line[len(line) // 2:]) for line in data]])
+part2 = sum(['abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.index(list(set(a).intersection(b).intersection(c))[0]) + 1 for a, b, c in [data[i:i+3] for i in range(0, len(data), 3)]])
+print(f'{part1=}, {part2=}')
