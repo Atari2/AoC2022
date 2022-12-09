95e87eea2314f6cb2b7f3ba281bcc4eeb36abc1f
Alessio Rosiello <alex.robassomero@gmail.com>
2022-12-09 14:28:44 +0100

    Performance improvements (around x3)

--- Day8/main.py
+++ Day8/main.py
@@ -8,35 +8,24 @@ with open(filename, 'r') as f:
 visible_count = 0
 scenic_score = 1
 tree_map = numpy.array([[int(x, 10) for x in line] for line in data])
-for i in range(len(tree_map)):
-    for j in range(len(tree_map[i])):
+rows, cols = tree_map.shape
+for i in range(rows):
+    for j in range(cols):
         num = tree_map[i,j]
-
-        left = tree_map[i,:j][::-1]
-        right = tree_map[i, j+1:]
-        top = tree_map[0: i, j][::-1]
-        bottom = tree_map[i+1:, j]
-
-        on_egde = False
+        slices = [tree_map[i,:j][::-1], tree_map[i, j+1:], tree_map[0: i, j][::-1], tree_map[i+1:, j]]
         # part 1
-        if any((len(x) == 0 for x in [left, right, top, bottom])):
-            visible_count += 1
-            on_egde = True
-        elif any((numpy.max(x) < num for x in [left, right, top, bottom])):
-            visible_count += 1
-        cur_tree_scores = []
+        visible_count += (on_edge := any((x.size == 0 for x in slices))) or any((x.max() < num for x in slices))
         # part 2
-        
-        if on_egde:
+        if on_edge:
             continue
-        for line in [left, right, top, bottom]:
-            score_line = line < num
-            if numpy.all(score_line):
-                cur_line_score = len(line)
+        cur_tree_scenic_score = 1
+        for line in slices:
+            score_line: numpy.ndarray = line < num
+            if score_line.all():
+                cur_line_score = line.size
             else:
-                cur_line_score = numpy.argmin(score_line) + 1
-            cur_tree_scores.append(cur_line_score)
-        cur_tree_scenic_score = numpy.prod(cur_tree_scores)
-        if cur_tree_scenic_score > scenic_score:
-            scenic_score = cur_tree_scenic_score
+                cur_line_score = int(score_line.argmin()) + 1
+            cur_tree_scenic_score *= cur_line_score
+        scenic_score = max(scenic_score, cur_tree_scenic_score)
+
 print(f'{visible_count} {scenic_score}')
